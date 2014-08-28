8d77471042d57046bedc436abac0f0e53a3a7cf9
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2014-08-28 17:07:34 +0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 Country.destroy_all
 
 Country.create(name: 'UK', proxy: '109.233.115.238:27463')
 Country.create(name: 'USA', proxy: '209.239.112.95:2589')
 
+queries = ['ios app makers',
+         'ios app makers in UK',
+         'ios app makers in USA',
+         'making an Android app',
+         'Android app makers',
+         'Android app Making',
+         'Android application maker',
+         'Android app Developers',
+         'Android app Developer company',
+         'Android app Developer service',
+         'Android application Developers',
+         'Android app design company',
+         'Android app design',
+         'Android application design',
+         'app builders',
+         'application builders',
+         'application builder company',
+         'app builder company',
+         'make an iPad app',
+         'making an iPad app',
+         'Mobile app design',
+         'Mobile app design services',
+         'Mobile application design service',
+         'application makers',
+         'corporate application design',
+         'enterprise application design',
+         'make an app',
+         'application Developer services',
+         'make an iPhone application',
+         'iphone app development',
+         'corporate iphone app developer',
+         'enterprise iphone app developer',
+         'iPad app design',
+         'iPad application Developers',
+         'application design agency',
+         'application design company',
+         'app development']
+
 Country.all.each do |country|
-  ['ios app makers', "ios app makers in #{country.name}"].each do |query|
+  queries.each do |query|
     Ad.create(body: query, country: country)
   end
 end
